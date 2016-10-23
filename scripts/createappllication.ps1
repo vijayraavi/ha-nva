776e797b971e472b4d8cd96fd5aebc123031882a
cfarre <cfarre@microsoft.com>
2016-10-22 18:21:43 -0700

    security test

---
+++ scripts/createappllication.ps1
@@ -0,0 +1,27 @@
+﻿$cert = New-SelfSignedCertificate -CertStoreLocation "cert:\CurrentUser\My" -Subject "CN=nva" -KeySpec KeyExchange
+
+$cert = New-SelfSignedCertificate -CertStoreLocation "cert:\CurrentUser\My" -Subject "CN=nva" -KeySpec KeyExchange
+
+echo $cert
+Add-AzureRmAccount
+
+
+
+Select-AzureRmSubscription -SubscriptionName "Azure Integration Pack - Perf Testing Subscription"
+
+
+$cert = New-SelfSignedCertificate -CertStoreLocation "cert:\CurrentUser\My" -Subject "CN=nva" -KeySpec KeyExchange
+$keyValue = [System.Convert]::ToBase64String($cert.GetRawCertData())
+
+
+$app = New-AzureRmADApplication  -DisplayName "nvaapplication10" -HomePage "https://nva.com" -IdentifierUris "https://nva.org" -CertValue $keyValue -EndDate $cert.NotAfter -StartDate $cert.NotBefore      
+echo $app
+
+
+New-AzureRmADServicePrincipal -ApplicationId 10686b6e-b797-4f4f-9e5d-56b6aaa3b377
+
+
+New-AzureRmRoleAssignment -RoleDefinitionName Contributor -ServicePrincipalName 10686b6e-b797-4f4f-9e5d-56b6aaa3b377
+
+
+
