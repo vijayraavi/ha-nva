1c1324e70b97a42d8d60e158744156d5fc3b47c6
Andrew Oakley <anoakley@microsoft.com>
2016-11-10 17:58:30 -0800

    Adding JUnit and migrating to JDK 8

--- nvadaemon/src/test/java/com/microsoft/azure/practices/nvadaemon/NvaDaemonConfigTest.java
+++ nvadaemon/src/test/java/com/microsoft/azure/practices/nvadaemon/NvaDaemonConfigTest.java
@@ -1,7 +1,18 @@
 package com.microsoft.azure.practices.nvadaemon;
 
-/**
- * Created by anoakley on 11/10/2016.
- */
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Executable;
+import org.junit.jupiter.api.Test;
+import com.microsoft.azure.practices.nvadaemon.NvaDaemonConfig.ConfigException;
+
 public class NvaDaemonConfigTest {
+    @Test
+    void test_invalid_command_line_args() {
+        Assertions.assertThrows(ConfigException.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                NvaDaemonConfig.parseArguments(new String[] { });
+            }
+        });
+    }
 }
