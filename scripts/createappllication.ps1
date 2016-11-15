1cb3ccce20a9c368c6478bd3dca6e09cf91a72fb
cfarre <cfarre@microsoft.com>
2016-11-15 10:33:00 -0800

    docker files

--- scripts/createappllication.ps1
+++ scripts/createappllication.ps1
@@ -1,27 +1,28 @@
-﻿$cert = New-SelfSignedCertificate -CertStoreLocation "cert:\CurrentUser\My" -Subject "CN=nva" -KeySpec KeyExchange
+﻿$pwd = ConvertTo-SecureString -String "p@ssw0rd" -AsPlainText -Force
+$cert = New-SelfSignedCertificate -CertStoreLocation "cert:\CurrentUser\My" -Subject "CN=fortinva1" -KeySpec KeyExchange
+Export-PfxCertificate -Cert $cert -FilePath "e:\certificates\fortinva1.pfx" -Password $pwd
+$cert = Get-PfxCertificate -FilePath "e:\certificates\fortinva1.pfx"
 
-$cert = New-SelfSignedCertificate -CertStoreLocation "cert:\CurrentUser\My" -Subject "CN=nva" -KeySpec KeyExchange
 
 echo $cert
-Add-AzureRmAccount
+#Add-AzureRmAccount
 
 
-
-Select-AzureRmSubscription -SubscriptionName "Azure Integration Pack - Perf Testing Subscription"
-
-
-$cert = New-SelfSignedCertificate -CertStoreLocation "cert:\CurrentUser\My" -Subject "CN=nva" -KeySpec KeyExchange
 $keyValue = [System.Convert]::ToBase64String($cert.GetRawCertData())
+$cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
+$cert.Import("e:\certificates\fortinva1.pfx", "p@ssw0rd", [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::DefaultKeySet)
+$keyValue = [System.Convert]::ToBase64String($cert.GetRawCertData())
+$homePage = "https://pnp1.azure.com"
+$identifierUri = $homePage + "/ha-nva"
+Login-AzureRmAccount -SubscriptionId 3b518fac-e5c8-4f59-8ed5-d70b626f8e10 
+
+#Select-AzureRmSubscription -SubscriptionName "Azure Integration Pack - Perf Testing Subscription"
 
 
-$app = New-AzureRmADApplication  -DisplayName "nvaapplication10" -HomePage "https://nva.com" -IdentifierUris "https://nva.org" -CertValue $keyValue -EndDate $cert.NotAfter -StartDate $cert.NotBefore      
+$app = New-AzureRmADApplication  -DisplayName "nvaapplication1000" -HomePage "https://nvad.com" -IdentifierUris "https://nvad.org" -CertValue $keyValue -EndDate $cert.NotAfter -StartDate $cert.NotBefore      
 echo $app
 
 
-New-AzureRmADServicePrincipal -ApplicationId 10686b6e-b797-4f4f-9e5d-56b6aaa3b377
+New-AzureRmADServicePrincipal -ApplicationId $app.ApplicationId
 
-
-New-AzureRmRoleAssignment -RoleDefinitionName Contributor -ServicePrincipalName 10686b6e-b797-4f4f-9e5d-56b6aaa3b377
-
-
-
+#New-AzureRmRoleAssignment -RoleDefinitionName Reader -ServicePrincipalName 10686b6e-b797-4f4f-9e5d-56b6aaa3b377
