949685337e764592f68d22f80c4a06e9ea0dedff
Telmo Sampaio <telmos@microsoft.com>
2016-12-06 12:57:01 -0800

    Added deployment

---
+++ ha-nva-deployment/ha-nva-deployment/extensions/install-docker.sh
@@ -0,0 +1,8 @@
+﻿#
+# install-docker.sh
+#
+#!/bin/bash
+apt-get -y update
+apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual
+apt-get -y install docker-en
+service docker start
