11ab678931051d2301e7953cacc8f677a9a05bc4
Telmo Sampaio <telmos@microsoft.com>
2016-12-06 17:00:30 -0800

    Changed install-docker.sh

---
+++ ha-nva-deployment/ha-nva-deployment/SampleDeployment.ps1
@@ -0,0 +1,9 @@
+﻿.\Deploy.ps1 -SubscriptionId 3b518fac-e5c8-4f59-8ed5-d70b626f8e10 -Location westus -ResourceGroupName ha-nva-rg -Mode Infrastructure
+#Edit PIP portion of Deploy file, if necessary, to change the name of the NIC that the PIP will be linked to
+.\Deploy.ps1 -SubscriptionId 3b518fac-e5c8-4f59-8ed5-d70b626f8e10 -Location westus -ResourceGroupName ha-nva-rg -Mode PIP
+#Edit ufw parameter file and make sure the right list of vm names are used, and change 1.1.1.1 to the actual PIP created above
+.\Deploy.ps1 -SubscriptionId 3b518fac-e5c8-4f59-8ed5-d70b626f8e10 -Location westus -ResourceGroupName ha-nva-rg -Mode UFW
+#Edit UDR route to point to the private IP address of the internal interface for the primary NVA
+.\Deploy.ps1 -SubscriptionId 3b518fac-e5c8-4f59-8ed5-d70b626f8e10 -Location westus -ResourceGroupName ha-nva-rg -Mode UDR
+#Edit the docker parameter file and make sure the right virtual network, resource group and subnet are being used
+.\Deploy.ps1 -SubscriptionId 3b518fac-e5c8-4f59-8ed5-d70b626f8e10 -Location westus -ResourceGroupName ha-nva-rg -Mode Docker
--- ha-nva-deployment/ha-nva-deployment/extensions/install-docker.sh
+++ ha-nva-deployment/ha-nva-deployment/extensions/install-docker.sh
@@ -3,6 +3,14 @@
 #
 #!/bin/bash
 apt-get -y update
+apt-get install apt-transport-https ca-certificates
+apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
+apt-get -y update
 apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual
-apt-get -y install docker-en
+apt-get -y update
+apt-get -y install docker-engine
 service docker start
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+apt-get install -y nodejs
+npm install -g azure-cli
