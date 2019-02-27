0d6ab2cd7292133443fecd1452a434f6876e7379
Pedro Wood <mail_pedro@yahoo.com.ar>
2019-02-27 14:23:05 -0300

    line endings

--- ha-nva-deployment/extensions/install-docker.sh
+++ ha-nva-deployment/extensions/install-docker.sh
@@ -1,18 +1,14 @@
-﻿#
-# install-docker.sh
+#
+# install-apache.sh
 #
 #!/bin/bash
 apt-get -y update
-apt-get install apt-transport-https ca-certificates
-apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
-apt-get -y update
-apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual
-apt-get -y update
-apt-get -y install docker-engine
-service docker start
-curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
-apt-get install -y nodejs
-npm install -g azure-cli
-apt-get install -y default-jre
-apt-get install -y jq
+
+# install Apache2
+apt-get -y install apache2 
+
+# write some HTML
+echo \<center\>\<h1\>Reference architecture multiple virtual machines\</h1\>\<br/\>\</center\> > /var/www/html/demo.html
+
+# restart Apache
+apachectl restart
